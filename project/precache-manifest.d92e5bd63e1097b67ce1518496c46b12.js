1cea5072f792d3bc0e7ba304bde52b9acdc667f0
yaohui.hou <yaohui.hou@qyer.com>
2020-04-20 00:58:42 +0800

    feat:project

---
+++ project/precache-manifest.d92e5bd63e1097b67ce1518496c46b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311d2a5e4cbbd170732820fd9fe14a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfcccf44631a6948abda",
+    "url": "/static/css/main.a65ee27b.chunk.css"
+  },
+  {
+    "revision": "fb0de3573dca023d1580",
+    "url": "/static/js/2.df928a91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df928a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcccf44631a6948abda",
+    "url": "/static/js/main.d69d7335.chunk.js"
+  },
+  {
+    "revision": "3c4b0cc570305db815fb",
+    "url": "/static/js/runtime-main.fee67b75.js"
+  }
+]);
